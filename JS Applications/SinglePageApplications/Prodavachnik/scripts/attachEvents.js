cbf2607103b7c7d7e87146f8933c5cb31efdae68
crespo <ktvalchev@gmail.com>
2018-08-13 00:00:03 +0300

    SPA: Prodavachnik

---
+++ JS Applications/SinglePageApplications/Prodavachnik/scripts/attachEvents.js
@@ -0,0 +1,52 @@
+function attachAllEvents() {
+    $("#linkHome").on('click', function () {
+        if (window.requestRunning === true) {
+            return;
+        }
+        showHomeView()
+    })
+    $("#linkLogin").on('click', function () {
+        if (window.requestRunning === true) {
+            return;
+        }
+        showLoginView()
+    })
+    $("#linkRegister").on('click', function () {
+        if (window.requestRunning === true) {
+            return;
+        }
+        showRegisterView();
+    })
+    $("#linkListAds").on('click', function () {
+        if (window.requestRunning === true) {
+            return;
+        }
+        listAds();
+    })
+    $("#linkCreateAd").on('click', function () {
+        if (window.requestRunning === true) {
+            return;
+        }
+        showCreateAdView();
+    })
+    $("#linkLogout").on('click', function () {
+        if (window.requestRunning === true) {
+            return;
+        }
+        logoutUser();
+    })
+
+    $("#buttonLoginUser").on('click', loginUser);
+    $("#buttonRegisterUser").on('click', registerUser);
+    $("#buttonCreateAd").on('click', createAd);
+    $("#buttonEditAd").on('click', editAd);
+
+    $("#infoBox, #errorBox").on('click', function() {
+        $(this).fadeOut();
+    })
+
+    $(document).on({
+        ajaxStart: function() { $("#loadingBox").show(), window.requestRunning = true},
+        ajaxStop: function() { $("#loadingBox").hide(), window.requestRunning = false}
+    })
+}
